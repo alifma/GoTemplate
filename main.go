150eaf3fa16ddf3ab0786998f874f3fc532ae156
alifma <alifmaulana26@gmail.com>
2021-03-27 14:59:49 +0700

    Add File Upload

--- main.go
+++ main.go
@@ -2,52 +2,78 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"text/template"
 )
 
 func routeIndexGet(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		var tmpl = template.Must(template.New("form").ParseFiles("form.html"))
-		var err = tmpl.Execute(w, nil)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+	if r.Method != "GET" {
+		http.Error(w, "", http.StatusBadRequest)
 		return
 	}
 
-	http.Error(w, "", http.StatusBadRequest)
+	var tmpl = template.Must(template.ParseFiles("view.html"))
+	var err = tmpl.Execute(w, nil)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		var tmpl = template.Must(template.New("result").ParseFiles("result.html"))
-
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		var name = r.FormValue("name")
-		var message = r.Form.Get("message")
-
-		var data = map[string]string{"name": name, "message": message}
-
-		if err := tmpl.Execute(w, data); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+	if r.Method != "POST" {
+		http.Error(w, "", http.StatusBadRequest)
 		return
 	}
 
-	http.Error(w, "", http.StatusBadRequest)
+	if err := r.ParseMultipartForm(1024); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	alias := r.FormValue("alias")
+
+	uploadedFile, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer uploadedFile.Close()
+
+	dir, err := os.Getwd()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filename := handler.Filename
+	if alias != "" {
+		filename = fmt.Sprintf("%s%s", alias, filepath.Ext(handler.Filename))
+	}
+	fileLocation := filepath.Join(dir, "files", filename)
+	targetFile, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer targetFile.Close()
+
+	if _, err := io.Copy(targetFile, uploadedFile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("done"))
 }
 
 func main() {
 	http.HandleFunc("/", routeIndexGet)
 	http.HandleFunc("/process", routeSubmitPost)
 
-	var port = ":9000"
-	fmt.Printf("Server is running on %s", port)
-	http.ListenAndServe(port, nil)
+	var addr = ":9000"
+	fmt.Printf("Service are started on localhost%s", addr)
+	http.ListenAndServe(addr, nil)
 }
