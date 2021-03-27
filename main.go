2063f7e8c7e44bb03296cb99d7a10a1795f3b96b
alifma <alifmaulana26@gmail.com>
2021-03-27 14:26:37 +0700

    Fix Base Form

--- main.go
+++ main.go
@@ -2,40 +2,52 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
+	"text/template"
 )
 
-type M map[string]interface{}
+func routeIndexGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var tmpl = template.Must(template.New("form").ParseFiles("form.html"))
+		var err = tmpl.Execute(w, nil)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var tmpl = template.Must(template.New("result").ParseFiles("result.html"))
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var name = r.FormValue("name")
+		var message = r.Form.Get("message")
+
+		var data = map[string]string{"name": name, "message": message}
+
+		if err := tmpl.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+}
 
 func main() {
-	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-		var data = M{"name": "Batman"}
-		var tmpl = template.Must(template.ParseFiles(
-			"views/index.html",
-			"views/_header.html",
-			"views/_message.html",
-		))
+	http.HandleFunc("/", routeIndexGet)
+	http.HandleFunc("/process", routeSubmitPost)
 
-		var err = tmpl.ExecuteTemplate(w, "index", data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		var data = M{"name": "Batman"}
-		var tmpl = template.Must(template.ParseFiles(
-			"views/about.html",
-			"views/_header.html",
-			"views/_message.html",
-		))
-
-		var err = tmpl.ExecuteTemplate(w, "about", data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	fmt.Println("server started at localhost:9000")
-	http.ListenAndServe(":9000", nil)
+	var port = ":9000"
+	fmt.Printf("Server is running on %s", port)
+	http.ListenAndServe(port, nil)
 }
